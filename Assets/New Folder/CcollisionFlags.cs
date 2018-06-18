52e47dd9997dd73724e0b01fe78188bb16e26f94
James Tichenor <tichenor.james@gmail.com>
2018-06-18 07:16:36 +0200

    adding files

---
+++ Assets/New Folder/CcollisionFlags.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class CcollisionFlags : MonoBehaviour {
+
+	public GameObject cloneScript;
+	//Scriptab
+	
+	void OnCollisionEnter(Collision collision){
+	//	Debug.Log("colliion started");
+
+	//cloneScript.GetComponent<AddElements>().makeObject();
+	}
+
+	void OnCollisionStay (Collision collision){
+	//	Debug.Log("Stay occuring..");
+	}
+
+	void OnCollisionExit (Collision collision)
+	{
+	//	Debug.Log("Exit called...");
+	//cloneScript.GetComponent<AddElements>().makeObject();
+	if (collision.gameObject.name == "floor")
+	{
+		cloneScript.GetComponent<AddElements>().makeObject();
+	}
+
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
