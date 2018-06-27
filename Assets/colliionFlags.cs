da9eac32a2e5177a6844f782071e7103e6f2c78c
james tichenor <jtichenor@rockwellgorup.com>
2018-06-27 14:41:19 -0700

    addedimages

---
+++ Assets/colliionFlags.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class colliionFlags : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+		void OnCollisionEnter(Collision collision){
+		Debug.Log("colliion started");
+	}
+
+	void OnCollisionStay (Collision collision){
+		Debug.Log("Stay occuring..");
+	}
+
+	void OnCollisionExit (Collision collision)
+	{
+		Debug.Log("Exit called...");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
